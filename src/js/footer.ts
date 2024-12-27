d5ba1dfc6d6b737b28f10f61da642794235d3d25
carolinearrestam <caroline@arrestam.se>
2024-12-27 12:36:09 +0100

    footer + style

---
+++ src/js/footer.ts
@@ -0,0 +1,20 @@
+import './footer.css'
+
+const subscribeButton = document.getElementById("subscribe-button") as HTMLButtonElement;
+const emailInput = document.getElementById("email-input") as HTMLInputElement;
+
+subscribeButton.addEventListener("click", () => {
+    const email = emailInput.value;
+
+    if (validateEmail(email)) {
+        alert(`BFF <3 Vi skickar nyhetsbrev till dig på, ${email}`);
+        emailInput.value = "";
+    } else {
+        alert("Vänligen ange en giltig e-postadress.");
+    }
+});
+
+function validateEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
