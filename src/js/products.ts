40f52b1ac6d2491479cce23bb43d78e908995e64
edch <soderblomed@gmail.com>
2024-12-27 13:43:55 +0100

    add stock status

--- src/js/products.ts
+++ src/js/products.ts
@@ -4,7 +4,7 @@ import getBooks from "./service/bookService.ts"
 
 
 export async function renderProductList(): Promise<void> {
-  const productList = document.querySelector(".product-list")
+  const productList = document.querySelector(".product-list") as HTMLUListElement
   const books = await getBooks();
   console.log(books);
   
@@ -13,15 +13,25 @@ export async function renderProductList(): Promise<void> {
   }
 }
 
-function createBookCard({id, title, author, cover, year, price, discount, stock}: IBook): string {
+// function getStockStatus(stock: number) {
+//   if (stock < 1) return {status: 4, message: "slut i lager"}
+//   if (stock < 20) return {status: 3, message: "få kvar i lager"}
+//   if (stock < 50 && stock > 20) return {status: 2, message: "20+ i lager"}
+//   if (stock > 50) return {status: 1, message: "50+ i lager"}
+// }
 
-  // const isDiscounted = discount && discount > 0;  
-  const discountPrice = calculateDiscount(price, discount)
-  const outOfStock = stock < 1;
+function createBookCard({id, title, author, cover, year, price, discount, stock}: IBook): string {
   
+  // const isDiscounted = discount && discount > 0;  
+  const discountPrice: number = calculateDiscount(price, discount)
+  const isOutOfStock: boolean = stock < 1;
+  // const stockStatus = getStockStatus(stock);
+  // console.log('status:', stockStatus);
+  
+
   const html = `
     <article class="product">
-    
+    ${discount ? `<div class="discount-badge"><span>du sparar</span><span>${price - discountPrice}kr</span></div>` : ""}
       <figure class="product-image">
         <a href="#">
           <img src="${cover}" alt="book cover of ${title} by ${printNames(author)}" />
@@ -38,30 +48,29 @@ function createBookCard({id, title, author, cover, year, price, discount, stock}
       <div class="product-price">
         <data class="${discount ? "strike" : ""}" value="${price}">${price}kr</data>    
         ${discount ? `<data value="${discountPrice}">${discountPrice}kr</data>` : ''}   
-        <div class="${stock > 0 ? "in-stock": "out-of-stock"} stock-indicator"></div>
+        <div class="${isOutOfStock ? "out-of-stock": "in-stock"} stock-indicator"></div>
       </div>
 
-      <div class="product-actions">
-        <button ${outOfStock && "disabled"} type="button" class="add-to-cart-button" data-x-son="x" data-book-id="${id}">Lägg till</button>
-        <button type="button" aria-label="Add to favorites" class="like-button" data-book-id="${id}">⭐️</button>
+      <div data-x="hello" class="product-actions">
+        <button ${isOutOfStock && "disabled"} type="button" class="add-to-cart-button" data-x-son="x" data-book-id="${id}">${isOutOfStock ? "Slut i lager" : "Lägg till"}</button>
+        <button type="button" aria-label="Add to favorites" class="add-to-fav-button" data-book-id="${id}">⭐️</button>
       </div>
     </article>
   `;
   return html;
-  // const container = document.querySelector('.product-list');
-  // container.insertAdjacentHTML('beforeend', html);
+
 }
 
 // Add event listeners after rendering
 document.addEventListener('click', (e) => {
  if(e.target && e.target instanceof HTMLElement) {
-   if (e.target.matches('.add-to-cart-button')) {
-    console.log(e.target.dataset);
+   if (e.target.parentElement && e.target.matches('.add-to-cart-button')) {
+    console.log(e.target.parentElement.dataset);
     
     const bookId = e.target.dataset.bookId;
     // handleAddToCart(bookId);
   }
-  if (e.target.matches('.like-button')) {
+  if (e.target.matches('.add-to-fav-button')) {
     const bookId = e.target.dataset.bookId;
     // handleLike(bookId);
   }
