53acba0209aba97f5651b6b75b007934f0a429d7
edch <soderblomed@gmail.com>
2024-12-31 12:39:59 +0100

    hero typewriter effect

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,33 @@
-import './styles/main.css'
-import { renderProductList } from './js/products.ts'
+import "./styles/main.css";
+import Typed from "typed.js";
 
-renderProductList()
+import { renderProductList } from "./js/products.ts";
 
+renderProductList();
+
+// hero typewriter
+var typed = new Typed(".typed", {
+  stringsElement: ".typed-strings",
+  typeSpeed: 30,
+});
 // typescript för footer
-const subscribeButton = document.getElementById("subscribe-button") as HTMLButtonElement;
+const subscribeButton = document.getElementById(
+  "subscribe-button"
+) as HTMLButtonElement;
 const emailInput = document.getElementById("email-input") as HTMLInputElement;
 
 subscribeButton.addEventListener("click", () => {
-    const email = emailInput.value;
+  const email = emailInput.value;
 
-    if (validateEmail(email)) {
-        alert(`BFF <3 Vi skickar nyhetsbrev till dig på, ${email}`);
-        emailInput.value = "";
-    } else {
-        alert("Vänligen ange en giltig e-postadress.");
-    }
+  if (validateEmail(email)) {
+    alert(`BFF <3 Vi skickar nyhetsbrev till dig på, ${email}`);
+    emailInput.value = "";
+  } else {
+    alert("Vänligen ange en giltig e-postadress.");
+  }
 });
 
 function validateEmail(email: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
