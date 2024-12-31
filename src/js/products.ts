535343e68eb06325d2b80a4d017505b64575c895
edch <soderblomed@gmail.com>
2024-12-31 10:36:50 +0100

    starDivs aria hidden

--- src/js/products.ts
+++ src/js/products.ts
@@ -189,6 +189,7 @@ export function renderProductList() {
 function createStarElement(starNumber: number) {
   const starDiv = document.createElement("div");
   starDiv.className = `star-${starNumber}`;
+  starDiv.ariaHidden = "true";
 
   // Create SVG element with all its attributes
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
