8e64e87c7f0d36f17ae77c30483c89c9c9c31b9c
edch <soderblomed@gmail.com>
2025-01-02 15:46:06 +0100

    css

--- src/js/favorites.ts
+++ src/js/favorites.ts
@@ -1,5 +1,10 @@
 import { IBook } from "./products";
 
+const favoritesPopover = document.querySelector(".favorites") as HTMLElement;
+const favPopoverBtn = document.querySelector(
+  "button[popovertarget='favorites']"
+) as HTMLButtonElement;
+
 export let favorites: IBook[] = JSON.parse(
   localStorage.getItem("favorites") || "[]"
 );
@@ -8,4 +13,12 @@ export function addToFavorites(book: IBook) {
   // kanske plocka ut 3-4 egenskaper och göra favorites till datatyp: IFavoriteBooks []
   favorites.push(book);
   localStorage.setItem("favorites", JSON.stringify(favorites));
+}
+
+favPopoverBtn?.addEventListener("click", updateFavoritesUI);
+
+function updateFavoritesUI() {
+  favoritesPopover.innerHTML = favorites
+    .map((fav) => `<div>${fav.title}</div>`)
+    .join("");
 }
