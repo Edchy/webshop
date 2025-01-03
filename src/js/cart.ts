fbbe4a1ce8bf0d2859fb8f6ad38d7c28f541e05c
edch <soderblomed@gmail.com>
2025-01-03 15:17:31 +0100

    total price

--- src/js/cart.ts
+++ src/js/cart.ts
@@ -1,13 +1,21 @@
 import { ICartBook } from "./Models/CartBook";
 import { IBook } from "./Models/Book";
+import { calculateTotal } from "./utils";
 
 // referenser
 export const cart: ICartBook[] = JSON.parse(
   localStorage.getItem("cart") || "[]"
 );
+const cartOpenBtn = document.querySelector(
+  "button[popovertarget='shopping-cart']"
+);
 const cartContainer = document.querySelector(".shopping-cart") as HTMLElement;
 const cartList = document.querySelector(".shopping-cart-list") as HTMLElement;
+const priceTotalOutput = document.querySelector(
+  ".shopping-cart-total"
+) as HTMLElement;
 //
+cartOpenBtn?.addEventListener("click", () => console.log("hi"));
 
 export function mapBookToCartBook(book: IBook): ICartBook {
   return {
@@ -49,27 +57,39 @@ export function renderCartUI() {
       cartList.append(cartItem);
     });
   }
+  if (priceTotalOutput) {
+    const total = calculateTotal(cart).toString();
+    priceTotalOutput.textContent = cart.length > 0 ? total : "";
+  }
 }
 
-function deleteCartItem() {}
+function removeFromCart(id: number) {
+  const indexToRemove = cart.findIndex((item) => item.id === id);
+  cart.splice(indexToRemove, 1);
+  renderCartUI();
+}
 
 function createCartItem(book: ICartBook) {
   const cartItem = document.createElement("li");
   cartItem.classList.add("cart-item");
+
   const cartItemImage = document.createElement("img");
   cartItemImage.src = book.cover;
   // cartItemImage.alt = `Omslag för ${book.title} av ${book.author}`;
   cartItem.appendChild(cartItemImage);
+
   const cartItemDetails = document.createElement("div");
   cartItemDetails.classList.add("cart-item-details");
   cartItemDetails.textContent = `${book.title} - ${book.price} kr`;
 
   const cartItemActions = document.createElement("div");
   cartItemActions.classList.add("cart-item-actions");
-  const cartItemQuantity = document.createElement("input") as HTMLInputElement;
+
   const cartItemRemove = document.createElement("button") as HTMLButtonElement;
   cartItemRemove.textContent = "❌";
+  cartItemRemove.addEventListener("click", () => removeFromCart(book.id));
 
+  const cartItemQuantity = document.createElement("input") as HTMLInputElement;
   cartItemQuantity.type = "number";
   cartItemQuantity.valueAsNumber = book.quantity;
 
